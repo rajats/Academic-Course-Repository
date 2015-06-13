80622cc33718acb4302013e2fb551a8dec00220e
Rajat <rajat_kumar@outlook.com>
2015-06-14 05:01:51 +0530

    implemented viewing of syllabus for instructor and student

--- course/views.py
+++ course/views.py
@@ -65,7 +65,31 @@ def add_assignment(request, id):
 				assignment = form.cleaned_data['assignment']
 				deadline = form.cleaned_data['deadline']
 				CourseAssignment.objects.create(course=course, description=description, assignment=assignment, deadline=deadline, timestamp=timezone.now())
-		return render_to_response("course/add_assignment.html", locals(), context_instance=RequestContext(request))
+		return render_to_response("course/addassignment.html", locals(), context_instance=RequestContext(request))
+	else:
+		raise Http404
+
+def view_syllabus(request, id):
+	if request.user.is_authenticated():
+		course = Course.objects.get(id=id)
+		syllabuses = CourseSyllabus.objects.filter(course=course)
+		if RegStudent.objects.filter(user=request.user.id).exists():
+			if RegStudent.objects.get(user=request.user).active:
+				reg_student = RegStudent.objects.get(user=request.user)
+				student = Student.objects.get(name=reg_student)
+				if course in student.courses.all():
+					return render_to_response("course/viewsyllabus.html", locals(), context_instance=RequestContext(request))
+				else:
+					raise Http404
+		elif RegProfessor.objects.filter(user=request.user.id).exists():
+			if RegProfessor.objects.get(user=request.user).active:
+				reg_professor = RegProfessor.objects.get(user=request.user)
+				if course in Course.objects.filter(instructor=reg_professor):
+					return render_to_response("course/viewsyllabus.html", locals(), context_instance=RequestContext(request))
+				else:
+					raise Http404
+		else:
+			raise Http404
 	else:
 		raise Http404
 
