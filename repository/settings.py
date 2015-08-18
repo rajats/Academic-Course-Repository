7671049a05cd777a52469bd87acc63e3483ac467
Rajat <rajat_kumar@outlook.com>
2015-08-18 20:46:37 +0530

    improved UI and used os.path.join in settings

--- repository/settings.py
+++ repository/settings.py
@@ -92,7 +92,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-MEDIA_ROOT = 'C:/Python27/django17/intshp/repository/static/media/'
+MEDIA_ROOT = os.path.join(os.path.dirname(os.path.dirname(__file__)), "static", "media") 
 
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
 
@@ -105,7 +105,7 @@ MEDIA_URL = '/static/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 
-STATIC_ROOT = 'C:/Python27/django17/intshp/repository/static/static-files/'
+STATIC_ROOT = os.path.join(os.path.dirname(os.path.dirname(__file__)), "static", "static-files")  
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -113,7 +113,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    ('C:/Python27/django17/intshp/repository/static/'),
+    os.path.join(os.path.dirname(os.path.dirname(__file__)), "static"),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
@@ -126,7 +126,7 @@ STATICFILES_FINDERS = (
 )
 
 TEMPLATE_DIRS = (
-    'C:/Python27/django17/intshp/repository/static/templates/',
+    os.path.join(os.path.dirname(os.path.dirname(__file__)), "static", "templates"), 
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
